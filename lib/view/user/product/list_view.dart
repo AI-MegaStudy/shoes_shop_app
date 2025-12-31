9d50517243de67a1a12adea35d082a89f71afa95
lambda2003 <35712663+lambda2003@users.noreply.github.com>
2025-12-31 18:33:02 +0900

    product page + detail page

--- lib/view/user/product/list_view.dart
+++ lib/view/user/product/list_view.dart
@@ -1,9 +1,10 @@
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shoes_shop_app/model/product.dart';
 import 'package:http/http.dart' as http;
+import 'package:shoes_shop_app/view/user/product/detail_view.dart';
 
 class ProductListView extends StatefulWidget {
   const ProductListView({super.key});
@@ -24,24 +25,19 @@ class _ProductListViewState extends State<ProductListView> {
   }
 
   Future<void> getProducts() async {
-
     // 요청하여 값을 가져온다.
     final url = Uri.parse("http://172.16.250.187:8000/api/products");
     final response = await http.get(url);
     final jsonData = json.decode(utf8.decode(response.bodyBytes));
 
-    
     products = jsonData["results"].map((d) => Product.fromJson(d)).toList();
-   
+    setState(() {});
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Page'),
-      ),
+      appBar: AppBar(title: Text('Product Page')),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -50,40 +46,44 @@ class _ProductListViewState extends State<ProductListView> {
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               crossAxisSpacing: 10, // 가로
-              mainAxisSpacing: 10,  // 세로
-            ), 
-            itemBuilder: (context, index){
+              mainAxisSpacing: 10, // 세로
+            ),
+            itemBuilder: (context, index) {
               return _displayProduct(products[index]);
-              
-            } 
+            },
           ),
         ),
-      )
+      ),
+
       // body:Center(
       //   // 물건 정보
-      //   child: 
+      //   child:
       //       ListView.builder(
       //       itemCount: products.length,
       //       itemBuilder: (context, index) {
       //           return _displayProduct(products[index]);
       //       },),
-       
+
       // )
-
     );
   }
 
-  Widget _displayProduct(Product p){
+  Widget _displayProduct(Product p) {
+    return GestureDetector(
+      onTap: () => Get.to(() => ProductDetailView(), arguments: p),
+      child: Card(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Image.asset(
+              'images/Nike_Air_1/Nike_Air_1_Black_01.avif',
+              fit: BoxFit.contain,
+            ),
 
-    return Card(
-      child: Column(
-        children: [
-          Text(p.pImage),
-          Text(p.pName)
-        ],
-      )
+            Column(children: [Text(p.pName), Text("${p.pPrice}")]),
+          ],
+        ),
+      ),
     );
-
   }
-
 }
